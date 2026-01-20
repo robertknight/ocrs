robertknight/ocrs

commit 75733b673e0dc42afa4e854af18ad02498ff1877

Update rten version to v0.24.0

ocrs/src/wasm_api.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
 
-use rten::ops;
-use rten::{Model, ModelOptions, OpRegistry};
+use rten::{op_registry, Model, ModelOptions, OpRegistry};
 
 use rten_imageproc::{min_area_rect, BoundingRect, PointF};
 use rten_tensor::prelude::*;
@@ -32,31 +31,29 @@ impl OcrEngineInit {
     }
 
     fn op_registry() -> OpRegistry {
-        let mut reg = OpRegistry::new();
-
         // Register all the operators the OCR models currently use.
-        reg.register_op::<ops::Add>();
-        reg.register_op::<ops::AveragePool>();
-        reg.register_op::<ops::Cast>();
-        reg.register_op::<ops::Concat>();
-        reg.register_op::<ops::ConstantOfShape>();
-        reg.register_op::<ops::Conv>();
-        reg.register_op::<ops::ConvTranspose>();
-        reg.register_op::<ops::GRU>();
-        reg.register_op::<ops::Gather>();
-        reg.register_op::<ops::LogSoftmax>();
-        reg.register_op::<ops::MatMul>();
-        reg.register_op::<ops::MaxPool>();
-        reg.register_op::<ops::Pad>();
-        reg.register_op::<ops::Relu>();
-        reg.register_op::<ops::Reshape>();
-        reg.register_op::<ops::Shape>();
-        reg.register_op::<ops::Sigmoid>();
-        reg.register_op::<ops::Slice>();
-        reg.register_op::<ops::Transpose>();
-        reg.register_op::<ops::Unsqueeze>();
-
-        reg
+        op_registry!(
+            Add,
+            AveragePool,
+            Cast,
+            Concat,
+            ConstantOfShape,
+            Conv,
+            ConvTranspose,
+            GRU,
+            Gather,
+            LogSoftmax,
+            MatMul,
+            MaxPool,
+            Pad,
+            Relu,
+            Reshape,
+            Shape,
+            Sigmoid,
+            Slice,
+            Transpose,
+            Squeeze
+        )
     }
 
     /// Load a model for text detection.

Cargo.toml
@@ -6,6 +6,6 @@ members = [
 ]
 
 [workspace.dependencies]
-rten = { version = "0.23.0", default-features = false, features = ["rten_format"] }
-rten-imageproc = { version = "0.23.0" }
-rten-tensor = { version = "0.23.0" }
+rten = { version = "0.24.0", default-features = false, features = ["rten_format"] }
+rten-imageproc = { version = "0.24.0" }
+rten-tensor = { version = "0.24.0" }

ocrs/src/preprocess.rs
@@ -222,7 +222,7 @@ fn convert_pixels<
 
         // We assume the input is likely contiguous, so this should be cheap.
         let src = src.to_contiguous();
-        let (src_pixels, remainder) = src.data().unwrap_if_needed().as_chunks::<PIXEL_STRIDE>();
+        let (src_pixels, remainder) = src.data().as_chunks::<PIXEL_STRIDE>();
         debug_assert!(remainder.is_empty());
 
         out_pixels.extend(src_pixels.iter().map(|in_pixel| {
@@ -261,32 +261,6 @@ impl AsF32 for f32 {
 impl AsF32 for u8 {
     fn as_f32(self) -> f32 {
         self as f32
-    }
-}
-
-/// Unwrap a value if contained in an [`Option`].
-///
-/// This trait exists to handle a change in the return type of
-/// [`NdTensorView::to_contiguous`] between rten-tensor v0.23 and later versions.
-trait UnwrapIfNeeded {
-    type Output;
-
-    fn unwrap_if_needed(self) -> Self::Output;
-}
-
-impl<T> UnwrapIfNeeded for &[T] {
-    type Output = Self;
-
-    fn unwrap_if_needed(self) -> Self::Output {
-        self
-    }
-}
-
-impl<T> UnwrapIfNeeded for Option<T> {
-    type Output = T;
-
-    fn unwrap_if_needed(self) -> T {
-        Self::unwrap(self)
     }
 }
 

commit 36f53c64f07a28c6a2209cde9613d0a693d621c1

Fix WebAssembly build
Fix a regression introduced in 75733b673e0dc42afa4e854af18ad02498ff1877 where
the list of enabled operators was incorrectly ported to the new API, replacing
`Unsqueeze` with `Squeeze`.

Fixes https://github.com/robertknight/ocrs/issues/227

ocrs/src/wasm_api.rs
@@ -52,7 +52,7 @@ impl OcrEngineInit {
             Sigmoid,
             Slice,
             Transpose,
-            Squeeze
+            Unsqueeze
         )
     }
 